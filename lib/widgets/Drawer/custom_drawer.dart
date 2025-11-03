1a774b5e848d2984772c1769c0e44c0739404c24
Omar Gamal <omarthefirst6206@gmail.com>
2025-11-03 23:05:14 +0200

    refactor: move UserInfoListTile to its own file

--- lib/widgets/Drawer/custom_drawer.dart
+++ lib/widgets/Drawer/custom_drawer.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:responsive_dashboard/utils/app_images.dart';
-import 'package:responsive_dashboard/utils/app_styles.dart';
+import 'package:responsive_dashboard/widgets/Drawer/user_info_list_tile.dart';
 
 class CustomDrawer extends StatelessWidget {
   const CustomDrawer({super.key});
@@ -18,29 +17,6 @@ class CustomDrawer extends StatelessWidget {
             subtitle: 'demo@gmail.com',
           ),
         ],
-      ),
-    );
-  }
-}
-
-class UserInfoListTile extends StatelessWidget {
-  const UserInfoListTile({
-    super.key,
-    required this.image,
-    required this.title,
-    required this.subtitle,
-  });
-  final String image, title, subtitle;
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      color: Color(0xffFAFAFA),
-      elevation: 0,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-      child: ListTile(
-        leading: SvgPicture.asset(image),
-        title: Text(title, style: AppStyles.styleSemiBold16),
-        subtitle: Text(subtitle, style: AppStyles.styleRegular12),
       ),
     );
   }
