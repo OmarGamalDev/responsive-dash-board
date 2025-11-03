c5a8de5a8ea0d97270a06f4cdd34fb4fed903979
Omar Gamal <omarthefirst6206@gmail.com>
2025-11-03 16:40:00 +0200

    Add SVG assets and update asset management in pubspec.yaml
    - Added new SVG images: avatar1.svg, avatar2.svg, avatar3.svg, balance.svg, card.svg, dashboard.svg, expenses.svg, income.svg, logout.svg, myInvestment.svg, myTransaction.svg, setting.svg, statistics.svg, and wallet.svg.
    - Updated pubspec.yaml to include the new assets directory and configured flutter_assets for automatic asset generation.
    - Created app_images.dart to manage image paths for easy access throughout the application.

---
+++ lib/utils/app_images.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  static const String imagesAvatar1 = "assets/images/avatar1.svg";
+
+  static const String imagesAvatar2 = "assets/images/avatar2.svg";
+
+  static const String imagesAvatar3 = "assets/images/avatar3.svg";
+
+  static const String imagesBalance = "assets/images/balance.svg";
+
+  static const String imagesCard = "assets/images/card.svg";
+
+  static const String imagesDashboard = "assets/images/dashboard.svg";
+
+  static const String imagesExpenses = "assets/images/expenses.svg";
+
+  static const String imagesIncome = "assets/images/income.svg";
+
+  static const String imagesLogout = "assets/images/logout.svg";
+
+  static const String imagesMyInvestment = "assets/images/myInvestment.svg";
+
+  static const String imagesMyTransaction = "assets/images/myTransaction.svg";
+
+  static const String imagesSetting = "assets/images/setting.svg";
+
+  static const String imagesStatistics = "assets/images/statistics.svg";
+
+  static const String imagesWallet = "assets/images/wallet.svg";
+}
+
